1e37321715f01590d5c8bb0d19ef73b3ca2f9d57
Samuel Prevost <samuel.prevost@pm.me>
2021-07-27 16:37:03 +0200

    Make setup.py ready for PyPI

--- setup.py
+++ setup.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python3
+import pathlib
+
 from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
 setup(
-    name = 'wattage',
-    version = '0.1.0',
-    packages = ['wattaged'],
-    entry_points = {
-        'console_scripts': [
-            'wattage = wattaged.main:main'
-        ]
-    })
+    name="wattaged",
+    version="0.1.0",
+    packages=["wattaged"],
+    description="wattaged is a utility that logs the consummed electricity and allows you to quickly compute the consummed kWh",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sam1902/wattaged",
+    author="Samuel Prevost",
+    author_email="samuel.prevost@pm.me",
+    licence="BSD",
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+    include_package_data=True,
+    install_requires=["pandas"],
+    entry_points={"console_scripts": ["wattage = wattaged.__main__:main"]},
+)
