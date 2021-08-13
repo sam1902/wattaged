88c9630b494a6f24fc2ed61390c14cbc1030ffb4
Samuel Prevost <samuel.prevost@pm.me>
2021-08-13 17:27:30 +0200

    Fix bug where invalid logs would be written
    Sometimes apcupsd would fail to return the statistics,
    that would make it impossible to compute the wattage, and
    it wouldn't write anything next to the date. Now, in case it can't
    get hold of the stats, the cron task just doesn't write
    anything down.

--- wattaged/wattage_apcupsd.sh
+++ wattaged/wattage_apcupsd.sh
@@ -4,7 +4,19 @@ if [ ! -d "/var/log/wattaged" ]; then
     mkdir -p /var/log/wattaged
 fi
 APC="/usr/sbin/apcaccess"
+
+test_is_num(){
+    re='^[0-9]+([.][0-9]+)?$'
+    if ! [[ "$1" =~ $re ]] ; then
+        echo "error: Not a number" >&2; exit 1
+    fi
+}
+load_pct=$($APC -p LOADPCT | cut -d' ' -f 1)
+nom_pwr=$($APC -p NOMPOWER | cut -d' ' -f 1)
+
+# Don't print anything if we couldn't get hold of the statistics
+test_is_num "$load_pct"
+test_is_num "$nom_pwr"
+
 echo -ne "$(date -u +'%Y-%m-%dT%H:%M:%S%Z')\t"
-bc <<< "scale=1; $($APC -p LOADPCT | cut -d' ' -f 1)*$($APC -p NOMPOWER | cut -d' ' -f 1)/100.0"
-
-
+bc <<< "scale=1; ${load_pct}*${nom_pwr}/100.0"
